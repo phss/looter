c298c70594e0ad012cc2b96e1930ab768e88cdbd
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-03 21:58:30 +0100

    Starting Sinatra app

---
+++ app/app.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+
+class LooterApp < Sinatra::Base
+
+  get '/' do
+    "Nothing to see here yet"
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+root = File.dirname(__FILE__)
+require File.join(root, 'app/app')
+
+run LooterApp.new
