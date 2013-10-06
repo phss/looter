78a58f245de782b949ef34bfa3225089d30a2679
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-06 21:30:12 +0100

    Add common starting describe block

--- spec/looter/game_spec.rb
+++ spec/looter/game_spec.rb
@@ -5,15 +5,20 @@ describe Game do
   let(:game) { Game.new(ui) }
   let(:sample_adventure) { double('adventure', :title => 'My great adventure') }
 
-  it 'displays main title when starting the game' do
-    game.play(sample_adventure)
 
-    ui.should have_received(:display_title).with('Looter')
-  end
+  describe '(starting)' do
 
-  it 'displays adventure title when starting the game' do
-    game.play(sample_adventure)
+    it 'displays main title' do
+      game.play(sample_adventure)
 
-    ui.should have_received(:display_subtitle).with('My great adventure')
+      ui.should have_received(:display_title).with('Looter')
+    end
+
+    it 'displays adventure title' do
+      game.play(sample_adventure)
+
+      ui.should have_received(:display_subtitle).with('My great adventure')
+    end
+
   end
 end
