ea1981abd6b6ab325415f3c49b250ce806d91c33
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-06 12:04:57 +0100

    Rename game to game_process

--- features/step_definitions/playing_steps.rb
+++ features/step_definitions/playing_steps.rb
@@ -1,10 +1,10 @@
 When(/^I start the game$/) do
-  @game = Game.new(IO.popen('./bin/game'))
+  @game_process = GameProcess.new(IO.popen('./bin/game'))
 end
 
 Then(/^I should see the options$/) do |table|
   expected_options = as_list(table)
-  actual_options = @game.read_options
+  actual_options = @game_process.read_options
 
   actual_options.should =~ expected_options
 end
@@ -13,7 +13,7 @@ def as_list(table)
   table.raw.map(&:first)
 end
 
-class Game
+class GameProcess
   def initialize(process)
     @process = process
   end
