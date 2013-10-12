fac354dbd3f79c244bce3509e725a8a3c0f101b1
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-12 08:49:09 +0100

    Validate display title

--- features/step_definitions/playing_steps.rb
+++ features/step_definitions/playing_steps.rb
@@ -2,6 +2,16 @@ When(/^I start the game$/) do
   @game_process = GameProcess.new(IO.popen('./bin/game'))
 end
 
+Then(/^I should see game title "(.*?)"$/) do |expected_title|
+  actual_title = @game_process.read_title
+
+  actual_title.should == expected_title
+end
+
+Then(/^I should see aventure title "(.*?)"$/) do |expected_title|
+  pending # express the regexp above with the code you wish you had
+end
+
 Then(/^I should see the options$/) do |table|
   expected_options = as_list(table)
   actual_options = @game_process.read_options
@@ -14,10 +24,16 @@ def as_list(table)
 end
 
 class GameProcess
+  TITLE_REGEXP = /^# (.*) #/
   OPTION_REGEXP = /^\d- (.*)/
 
   def initialize(process)
     @process = process
+  end
+
+  def read_title
+    option_lines = output_lines.select { |line| TITLE_REGEXP.match(line) }
+    return option_lines.map { |line| TITLE_REGEXP.match(line)[1] }.first
   end
 
   def read_options
