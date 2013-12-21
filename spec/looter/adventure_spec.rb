63da9d3f627130ec545fb99ede2c96630115ab40
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-21 21:53:54 -0200

    Pending test for adding rooms to Adventure

--- spec/looter/adventure_spec.rb
+++ spec/looter/adventure_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe Adventure do
 
+  it 'holds a many rooms' do
+    pending
+    adventure = Adventure.new('Testing rooms', [
+      Room.new(:first,  'This is first'),
+      Room.new(:second, 'Segundo'),
+      Room.new(:last,   'Last one'),
+    ])
+
+    adventure.room(:first).name.should == 'This is first'
+    adventure.room(:second).name.should == 'Segundo'
+    adventure.room(:last).name.should == 'Last one'
+  end
+
   it 'loads title from configuration hash' do
     adventure = Adventure.from_hash({'title' => 'Title from hash'})
 
