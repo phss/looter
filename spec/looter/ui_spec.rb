c507a8750ac361a18c41c0157b41ce8fcb11304d
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-24 19:06:31 -0200

    Move screen spec to it's own describe

--- spec/looter/ui_spec.rb
+++ spec/looter/ui_spec.rb
@@ -5,19 +5,23 @@ describe UI do
   let(:output) { StringIO.new }
   let(:ui) { UI.new(input, output) }
 
-  it 'displays screen' do
-    ui.display_screen(
-      :title => 'My title',
-      :subtitle => 'Your subtitle',
-      :options => ['One', 'Two'])
-
-    output.string.should == "# My title #\n- Your subtitle -\n1- One\n2- Two\n"
-  end
-
   it 'displays a message' do
     ui.display_message('Some message')
 
     output.string.should == "Some message\n"
+  end
+
+  describe '(displaying a screen)' do
+
+    it 'displays screen' do
+      ui.display_screen(
+        :title => 'My title',
+        :subtitle => 'Your subtitle',
+        :options => ['One', 'Two'])
+
+      output.string.should == "# My title #\n- Your subtitle -\n1- One\n2- Two\n"
+    end
+
   end
 
   describe '(choosing an option)' do
