47c692f3f6287c77cb36e1e7785cde2c757d4b09
Paulo Schneider <paulo.schneider@gmail.com>
2014-03-09 19:02:56 +0000

    Extract intro and room screens

--- lib/looter/game.rb
+++ lib/looter/game.rb
@@ -2,31 +2,64 @@ class Game
 
   def initialize(ui)
     @ui = ui
+    @screens = {
+      :intro => IntroScreen.new(ui),
+      :room => RoomScreen.new(ui)
+    }
+    @current_screen = :intro
   end
 
   def play(adventure)
-    option = main_menu_for(adventure)
+    while @current_screen
+      screen = @screens[@current_screen]
+      screen.show(adventure)
+      @current_screen = screen.next_screen
+    end
+  end
+
+end
+
+class IntroScreen
+
+  def initialize(ui)
+    @ui = ui
+  end
+
+  def show(adventure)
+    @ui.display_screen(
+      :title => 'Looter',
+      :subtitle => adventure.title,
+      :options => ['Start new game', 'Exit'])
+  end
+
+  def next_screen
+    option = @ui.choose_option([:start, :exit])
 
     if option == :exit
       @ui.display_screen(:description => "Bye!")
-      return
+      return nil
     end
 
+    return :room
+  end
+
+end
+
+class RoomScreen
+  
+  def initialize(ui)
+    @ui = ui
+  end
+
+  def show(adventure)
     @ui.display_screen(
       :title => adventure.next_room.name,
       :description => adventure.next_room.description,
       :options => adventure.next_room.exit_names)
   end
 
- private
-
-  def main_menu_for(adventure)
-    @ui.display_screen(
-      :title => 'Looter',
-      :subtitle => adventure.title,
-      :options => ['Start new game', 'Exit'])
-
-    @ui.choose_option([:start, :exit])
+  def next_screen
+    nil
   end
 
 end
