680d6d79bf369c24f53df15a5bbc2122e0469e8f
Paulo Schneider <paulo.schneider@gmail.com>
2014-03-17 20:28:21 +0000

    Save current room from adventure

--- lib/looter/saver.rb
+++ lib/looter/saver.rb
@@ -1,12 +1,15 @@
 
 class AdventureSaver
 
-  def initialize()
-    
+  def initialize(save_filename)
+    @save_filename = save_filename
   end
 
   def save(adventure)
-
+    save_contents = {
+      'current_room' => adventure.current_room.id
+    }
+    File.open(@save_filename, 'w') { |f| YAML.dump(save_contents, f) }
   end
 
 end
---
+++ spec/looter/ui/saver_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'YAML'
+
+describe AdventureSaver do
+
+  it 'saves current room from adventure' do
+    adventure = Adventure.new('Testing rooms', [
+      Room.new(:start, 'First', nil, [ Exit.new(:left, "Left"), Exit.new(:right, "Right") ]),
+      Room.new(:left, 'Left', nil, [ Exit.new(:another, "Another") ]),
+      Room.new(:right, 'Right', nil, [ Exit.new(:no_way, "No way") ]),
+      Room.new(:another, 'Final destination', nil, [])
+    ])
+    adventure.travel_to(:right)
+
+    saver = AdventureSaver.new('/tmp/save_file.yaml')
+    saver.save(adventure)
+
+    YAML.load_file('/tmp/save_file.yaml').should == { 'current_room' => :right }
+  end
+
+end
