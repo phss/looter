32ae641281b8ba81e8a095383d00e6676d9ba7d9
Paulo Schneider <paulo.schneider@gmail.com>
2014-04-05 12:30:06 +0100

    Extract adventure hash to let

--- spec/looter/loader_spec.rb
+++ spec/looter/loader_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
 
 describe AdventureLoader do
-
-  it 'loads from configuration hash' do
-    adventure = AdventureLoader.load_from_hash({
+  let(:adventure_hash) do
+    {
       'title' => 'Title from hash',
       'rooms' => [
         { 'id' => 'start',   'name' => 'First room', 'directions' => [] },
         { 'id' => 'another', 'name' => 'Another room', 'directions' => [] }
       ]
-    })
+    }
+  end
+
+  it 'loads from configuration hash defaulting current room to start when no state hash' do
+    adventure = AdventureLoader.load_from_hash(adventure_hash)
 
     adventure.title.should == 'Title from hash'
     adventure.room(:start).name.should == 'First room'
     adventure.room(:another).name.should == 'Another room'
+    adventure.current_room.name.should == 'First room'
   end
 
 end
