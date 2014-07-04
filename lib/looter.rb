45f3263d5da52782a08034462021b45ed7f07b5a
Paulo Schneider <paulo.schneider@gmail.com>
2014-07-04 14:17:44 +0100

    Removing all ruby stuff, prior to starting from scratch

--- lib/looter.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'looter/loader'
-require_relative 'looter/game_persistence'
-require_relative 'looter/game'
-require_relative 'looter/adventure'
-require_relative 'looter/room'
-require_relative 'looter/ui'
-require_relative 'looter/screens'
